4f8e1c82da493ac01eb183d6a39a4dd62c15efbb
Alex Nichol <unixpickle@gmail.com>
2016-08-23 20:56:18 -0400

    queue gradients work

--- queue.go
+++ queue.go
@@ -44,6 +44,9 @@ type queueState struct {
 	Expected   []linalg.Vector
 	SizeProbs  []float64
 	OutputData linalg.Vector
+
+	ControlIn linalg.Vector
+	Last      *queueState
 }
 
 func (q *queueState) Data() linalg.Vector {
@@ -51,8 +54,78 @@ func (q *queueState) Data() linalg.Vector {
 }
 
 func (q *queueState) Gradient(dataGrad linalg.Vector, upstreamGrad Grad) (linalg.Vector, Grad) {
-	// TODO: this.
-	return make(linalg.Vector, len(dataGrad)+queueFlagCount), nil
+	if q.Last == nil {
+		panic("cannot propagate through start state")
+	}
+	softmax := autofunc.Softmax{}
+	flagsVar := &autofunc.Variable{Vector: q.ControlIn[:queueFlagCount]}
+	flagRes := softmax.Apply(flagsVar)
+	flags := flagRes.Output()
+
+	flagsGrad := make(linalg.Vector, queueFlagCount)
+
+	var upstream *queueUpstream
+	if upstreamGrad != nil {
+		upstream = upstreamGrad.(*queueUpstream)
+		upstream.Expected[0].Add(dataGrad)
+	} else {
+		upstream = new(queueUpstream)
+		upstream.SizeProbs = make([]float64, len(q.SizeProbs))
+		upstream.Expected = make([]linalg.Vector, len(q.Expected))
+		upstream.Expected[0] = dataGrad
+		zeroVec := make(linalg.Vector, len(dataGrad))
+		for i := 1; i < len(upstream.Expected); i++ {
+			upstream.Expected[i] = zeroVec
+		}
+	}
+
+	downstream := &queueUpstream{
+		Expected:  make([]linalg.Vector, len(q.Last.Expected)),
+		SizeProbs: make([]float64, len(q.Last.SizeProbs)),
+	}
+	for i, vec := range q.Last.Expected {
+		downstream.Expected[i] = upstream.Expected[i].Copy().Scale(flags[queueNop] +
+			flags[queuePush])
+		flagsGrad[queueNop] += vec.Dot(upstream.Expected[i])
+		flagsGrad[queuePush] += vec.Dot(upstream.Expected[i])
+		if i > 0 {
+			downstream.Expected[i].Add(upstream.Expected[i-1].Copy().Scale(flags[queuePop]))
+			flagsGrad[queuePop] += vec.Dot(upstream.Expected[i-1])
+		}
+	}
+
+	pushDataGrad := make(linalg.Vector, len(q.Data()))
+
+	for i, prob := range q.Last.SizeProbs {
+		pushData := q.ControlIn[queueFlagCount:]
+		pushDataGrad.Add(upstream.Expected[i].Copy().Scale(flags[queuePush] * prob))
+		upstreamDot := upstream.Expected[i].Dot(pushData)
+		flagsGrad[queuePush] += prob * upstreamDot
+		downstream.SizeProbs[i] += flags[queuePush] * upstreamDot
+	}
+
+	for i, old := range q.Last.SizeProbs {
+		downstream.SizeProbs[i] += flags[queueNop] * upstream.SizeProbs[i]
+		flagsGrad[queueNop] += old * upstream.SizeProbs[i]
+		if i > 0 {
+			downstream.SizeProbs[i] += flags[queuePop] * upstream.SizeProbs[i-1]
+			flagsGrad[queuePop] += old * upstream.SizeProbs[i-1]
+		} else {
+			downstream.SizeProbs[i] += flags[queuePop] * upstream.SizeProbs[i]
+			flagsGrad[queuePop] += old * upstream.SizeProbs[i]
+		}
+		downstream.SizeProbs[i] += flags[queuePush] * upstream.SizeProbs[i+1]
+		flagsGrad[queuePush] += old * upstream.SizeProbs[i+1]
+	}
+
+	fg := autofunc.NewGradient([]*autofunc.Variable{flagsVar})
+	flagRes.PropagateGradient(flagsGrad, fg)
+
+	ctrlGrad := make(linalg.Vector, queueFlagCount+len(pushDataGrad))
+	copy(ctrlGrad, fg[flagsVar])
+	copy(ctrlGrad[queueFlagCount:], pushDataGrad)
+
+	return ctrlGrad, downstream
 }
 
 func (q *queueState) NextState(ctrl linalg.Vector) State {
@@ -93,6 +166,13 @@ func (q *queueState) NextState(ctrl linalg.Vector) State {
 	}
 
 	res.OutputData = res.Expected[0]
+	res.ControlIn = ctrl
+	res.Last = q
 
 	return &res
+}
+
+type queueUpstream struct {
+	Expected  []linalg.Vector
+	SizeProbs []float64
 }
