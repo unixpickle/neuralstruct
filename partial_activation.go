7f175db042cb1b68ddc0ca8a62fb6bd7450fbd67
Alex Nichol <unixpickle@gmail.com>
2016-08-26 18:14:42 -0400

    partial activation supports multiple parts
    This is an important breaking change. It will make it possible to efficiently use PartialActivation with a compounded, aggregate structure.

--- partial_activation.go
+++ partial_activation.go
@@ -1,7 +1,9 @@
 package neuralstruct
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 
 	"github.com/unixpickle/autofunc"
 	"github.com/unixpickle/serializer"
@@ -13,24 +15,32 @@ func init() {
 	serializer.RegisterTypedDeserializer(p.SerializerType(), DeserializePartialActivation)
 }
 
+// ComponentRange specifies a range of components inside
+// a vector.
+type ComponentRange struct {
+	// Start is the (inclusive) start index.
+	Start int
+
+	// End is the (exclusive) end index.
+	End int
+}
+
 // PartialActivation is an activation function which
-// squashes a sub-range of its inputs using an activation
+// "squashes" sub-ranges of its inputs with an activation
 // function while leaving the rest untouched.
 //
 // This is useful for squashing the data part of a control
 // signal in a SeqFunc.
 type PartialActivation struct {
-	// SquashStart is the start index (inclusive) of the
-	// activation part of input vectors.
-	SquashStart int
+	// Ranges is the list of ranges to squash, in ascending
+	// order of start index.
+	// This must be sorted; otherwise, the behavior of a
+	// PartialActivation is undefined.
+	Ranges []ComponentRange `json:"ranges"`
 
-	// SquashEnd is the end index (exclusive) of the
-	// activation part of input vectors.
-	SquashEnd int
-
-	// Activation is the activation function to partially
-	// apply to input vectors.
-	Activation neuralnet.Layer
+	// Activations are the activation functions, one per
+	// each range in Ranges.
+	Activations []neuralnet.Layer `json:"-"`
 }
 
 // DeserializePartialActivation deserializes a PartialActivation.
@@ -39,47 +49,82 @@ func DeserializePartialActivation(d []byte) (*PartialActivation, error) {
 	if err != nil {
 		return nil, err
 	}
-	if len(slice) != 3 {
+	if len(slice) == 0 {
 		return nil, errors.New("invalid PartialActivation slice")
 	}
-	start, ok1 := slice[0].(serializer.Int)
-	end, ok2 := slice[1].(serializer.Int)
-	activation, ok3 := slice[2].(neuralnet.Layer)
-	if !ok1 || !ok2 || !ok3 {
-		return nil, errors.New("invalid PartialActivation slice")
+	jsonData, ok := slice[0].(serializer.Bytes)
+	if !ok {
+		return nil, errors.New("invalid PartialActivation slice (entry 0)")
 	}
-	return &PartialActivation{
-		SquashStart: int(start),
-		SquashEnd:   int(end),
-		Activation:  activation,
-	}, nil
+	var res PartialActivation
+	if err := json.Unmarshal(jsonData, &res); err != nil {
+		return nil, fmt.Errorf("invalid PartialActivation JSON: %s", err)
+	}
+	for i, activationObj := range slice[1:] {
+		activation, ok := activationObj.(neuralnet.Layer)
+		if !ok {
+			return nil, fmt.Errorf("invalid PartialActivation slice (entry %d)", i+1)
+		}
+		res.Activations = append(res.Activations, activation)
+	}
+	return &res, nil
 }
 
 // Apply applies the activation function to an input.
 func (p *PartialActivation) Apply(rawIn autofunc.Result) autofunc.Result {
-	if len(rawIn.Output()) < p.SquashEnd {
+	if len(p.Ranges) == 0 {
+		return rawIn
+	}
+	if len(p.Ranges) != len(p.Activations) {
+		panic("mismatching number of ranges and activations")
+	}
+	if len(rawIn.Output()) < p.Ranges[len(p.Ranges)-1].End {
 		panic("input vector is too short")
 	}
 	return autofunc.Pool(rawIn, func(in autofunc.Result) autofunc.Result {
-		preSquash := autofunc.Slice(in, 0, p.SquashStart)
-		squash := autofunc.Slice(in, p.SquashStart, p.SquashEnd)
-		postSquash := autofunc.Slice(in, p.SquashEnd, len(in.Output()))
-		squash = p.Activation.Apply(squash)
-		return autofunc.Concat(preSquash, squash, postSquash)
+		var slices []autofunc.Result
+		var lastCovered int
+		for i, r := range p.Ranges {
+			if r.Start > lastCovered {
+				slices = append(slices, autofunc.Slice(in, lastCovered, r.Start))
+			}
+			squashMe := autofunc.Slice(in, r.Start, r.End)
+			slices = append(slices, p.Activations[i].Apply(squashMe))
+			lastCovered = r.End
+		}
+		if lastCovered < len(in.Output()) {
+			slices = append(slices, autofunc.Slice(in, lastCovered, len(in.Output())))
+		}
+		return autofunc.Concat(slices...)
 	})
 }
 
 // ApplyR is like Apply but with r-operator support.
 func (p *PartialActivation) ApplyR(rv autofunc.RVector, rawIn autofunc.RResult) autofunc.RResult {
-	if len(rawIn.Output()) < p.SquashEnd {
+	if len(p.Ranges) == 0 {
+		return rawIn
+	}
+	if len(p.Ranges) != len(p.Activations) {
+		panic("mismatching number of ranges and activations")
+	}
+	if len(rawIn.Output()) < p.Ranges[len(p.Ranges)-1].End {
 		panic("input vector is too short")
 	}
 	return autofunc.PoolR(rawIn, func(in autofunc.RResult) autofunc.RResult {
-		preSquash := autofunc.SliceR(in, 0, p.SquashStart)
-		squash := autofunc.SliceR(in, p.SquashStart, p.SquashEnd)
-		postSquash := autofunc.SliceR(in, p.SquashEnd, len(in.Output()))
-		squash = p.Activation.ApplyR(rv, squash)
-		return autofunc.ConcatR(preSquash, squash, postSquash)
+		var slices []autofunc.RResult
+		var lastCovered int
+		for i, r := range p.Ranges {
+			if r.Start > lastCovered {
+				slices = append(slices, autofunc.SliceR(in, lastCovered, r.Start))
+			}
+			squashMe := autofunc.SliceR(in, r.Start, r.End)
+			slices = append(slices, p.Activations[i].ApplyR(rv, squashMe))
+			lastCovered = r.End
+		}
+		if lastCovered < len(in.Output()) {
+			slices = append(slices, autofunc.SliceR(in, lastCovered, len(in.Output())))
+		}
+		return autofunc.ConcatR(slices...)
 	})
 }
 
@@ -91,10 +136,13 @@ func (p *PartialActivation) SerializerType() string {
 
 // Serialize serializes the PartialActivation.
 func (p *PartialActivation) Serialize() ([]byte, error) {
-	slice := []serializer.Serializer{
-		serializer.Int(p.SquashStart),
-		serializer.Int(p.SquashEnd),
-		p.Activation,
+	jsonData, err := json.Marshal(p)
+	if err != nil {
+		return nil, err
+	}
+	slice := []serializer.Serializer{serializer.Bytes(jsonData)}
+	for _, act := range p.Activations {
+		slice = append(slice, act)
 	}
 	return serializer.SerializeSlice(slice)
 }
--- partial_activation_test.go
+++ partial_activation_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/unixpickle/autofunc"
+	"github.com/unixpickle/serializer"
 	"github.com/unixpickle/weakai/neuralnet"
 )
 
@@ -16,13 +17,18 @@ func TestPartialActivation(t *testing.T) {
 	}
 
 	inputs := []float64{-1, 10, 5, -2, 3, 1, -2, 5}
-	expected := []float64{-1, 10, squash(5), squash(-2), squash(3), 1, -2, 5}
+	expected := []float64{-1, 10, squash(5), squash(-2), squash(3), 1, -2, math.Tanh(5)}
 
 	inVar := &autofunc.Variable{Vector: inputs}
 	activation := &PartialActivation{
-		SquashStart: 2,
-		SquashEnd:   5,
-		Activation:  &neuralnet.Sigmoid{},
+		Ranges: []ComponentRange{
+			{2, 5},
+			{7, 8},
+		},
+		Activations: []neuralnet.Layer{
+			&neuralnet.Sigmoid{},
+			&neuralnet.HyperbolicTangent{},
+		},
 	}
 	actual := activation.Apply(inVar).Output()
 
@@ -35,5 +41,39 @@ func TestPartialActivation(t *testing.T) {
 		if math.Abs(x-a) > 1e-5 {
 			t.Errorf("entry %d should be %f but it's %f", i, x, a)
 		}
+	}
+}
+
+func TestPartialActivationSerialize(t *testing.T) {
+	activation := &PartialActivation{
+		Ranges: []ComponentRange{
+			{2, 5},
+			{7, 8},
+		},
+		Activations: []neuralnet.Layer{
+			&neuralnet.Sigmoid{},
+			&neuralnet.HyperbolicTangent{},
+		},
+	}
+	data, err := serializer.SerializeWithType(activation)
+	if err != nil {
+		t.Fatal(err)
+	}
+	newActivation, err := serializer.DeserializeWithType(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	act, ok := newActivation.(*PartialActivation)
+	if !ok {
+		t.Fatalf("type shouldn't be %T", newActivation)
+	}
+	if len(act.Ranges) != 2 || len(act.Activations) != 2 {
+		t.Fatalf("lens are %d,%d not 2,2", len(act.Ranges), len(act.Activations))
+	}
+
+	_, ok1 := act.Activations[0].(*neuralnet.Sigmoid)
+	_, ok2 := act.Activations[1].(*neuralnet.HyperbolicTangent)
+	if !ok1 || !ok2 {
+		t.Fatalf("types shouldn't be %T,%T", act.Activations[0], act.Activations[1])
 	}
 }
